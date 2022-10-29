d48ac22ee7615039e79707ac6fb18a1a4d28df2d
Willy <willychai05@gmail.com>
2022-10-29 15:14:05 +0700

    simpel vulkan window

---
+++ learnvulkan/src/main.rs
@@ -0,0 +1,36 @@
+use vulkano::{
+    instance::{Instance, InstanceCreateInfo},
+    VulkanLibrary,
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
+fn main() {
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    let required_extensions = vulkano_win::required_extensions(&library);
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            enabled_extensions: required_extensions,
+            ..Default::default()
+        },
+    )
+    .expect("failed to create instance");
+
+    let event_loop = EventLoop::new(); // ignore this for now
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    event_loop.run(|event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        _ => (),
+    });
+}
